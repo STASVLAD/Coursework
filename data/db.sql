4e4eed32f394f768034294a453beca85bd47d045
Nikita Pachshenko <nikitapvl97@mail.ru>
2021-06-20 05:07:22 +0300

    add new eda.ru dataset

--- data/db.sql
+++ data/db.sql
@@ -6,10 +6,11 @@ CREATE TABLE public.shopping_list
 (
     id integer NOT NULL DEFAULT nextval('shopping_list_id_seq'::regclass),
     user_id character varying(64) COLLATE pg_catalog."default" NOT NULL,
-    product character varying(40) COLLATE pg_catalog."default",
+    product character varying(64) COLLATE pg_catalog."default",
     quantity integer,
     created_on timestamp(0) without time zone DEFAULT CURRENT_TIMESTAMP,
-    frequency interval hour,
+    units character varying(64) COLLATE pg_catalog."default",
+    frequency interval day[],
     CONSTRAINT shopping_list_pkey PRIMARY KEY (id),
     CONSTRAINT shopping_list_user_id_product_key UNIQUE (user_id, product)
 )
@@ -19,24 +20,20 @@ TABLESPACE pg_default;
 ALTER TABLE public.shopping_list
     OWNER to postgres;
 
+-----------------------------------------------------------------------------
 
--- Table: public.shopping_list
+-- Table: public.product_prices
 
--- DROP TABLE public.shopping_list;
+-- DROP TABLE public.product_prices;
 
-CREATE TABLE public.shopping_list
+CREATE TABLE public.product_prices
 (
-    id integer NOT NULL DEFAULT nextval('shopping_list_id_seq'::regclass),
-    user_id character varying(64) COLLATE pg_catalog."default" NOT NULL,
-    product character varying(40) COLLATE pg_catalog."default",
-    quantity integer,
-    created_on timestamp(0) without time zone DEFAULT CURRENT_TIMESTAMP,
-    frequency interval hour,
-    CONSTRAINT shopping_list_pkey PRIMARY KEY (id),
-    CONSTRAINT shopping_list_user_id_product_key UNIQUE (user_id, product)
+    product character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    price integer,
+    CONSTRAINT product_prices_pkey PRIMARY KEY (product)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public.shopping_list
+ALTER TABLE public.product_prices
     OWNER to postgres;
